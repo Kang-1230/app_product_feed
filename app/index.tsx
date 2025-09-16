f16de7fd7f22080aab347a44212b934a9a84e80a
KangSujin <jin8898344@gmail.com>
2025-09-16 17:41:41 +0900

    Revert "pull 충돌 수정"
    This reverts commit 5696cbd925f45f588e3f1c30a870f71261f1b1e8, reversing
    changes made to abbf46988767aaabc0bfa25c185a0719bf3c5c4c.

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ProductCard from "@/components/ProductCard";
 import { getProducts } from "@/lib/api";
 import { ProductItem } from "@/type/productItem";
@@ -20,20 +19,8 @@ const Index = () => {
         data={products}
         renderItem={({ item }) => <ProductCard item={item} />}
       />
-=======
-import { Text, View } from "react-native";
-
-const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
->>>>>>> b882be43431daaf0121441b2e4428564f774726a
     </View>
   );
 };
 
-<<<<<<< HEAD
 export default Index;
-=======
-export default index;
->>>>>>> b882be43431daaf0121441b2e4428564f774726a
