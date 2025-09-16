a5358336a38870e850c75b16175d774c33d22a13
KangSujin <jin8898344@gmail.com>
2025-09-16 17:41:05 +0900

    Revert "충돌 수정"
    This reverts commit b227a1f5be027a3bdd297214fb374b077f96ad59.

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import ProductCard from "@/components/ProductCard";
 import { getProducts } from "@/lib/api";
 import { ProductItem } from "@/type/productItem";
@@ -19,8 +20,20 @@ const Index = () => {
         data={products}
         renderItem={({ item }) => <ProductCard item={item} />}
       />
+=======
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+>>>>>>> b882be43431daaf0121441b2e4428564f774726a
     </View>
   );
 };
 
+<<<<<<< HEAD
 export default Index;
+=======
+export default index;
+>>>>>>> b882be43431daaf0121441b2e4428564f774726a
